c979a5309efb24df26d89bb1009208e93dd41630
Allan Lopes <lopes.shigame@gmail.com>
2025-11-14 10:36:47 -0300

    delete

--- Locadoraveículos/src/main/java/br/senac/sp/Locadoraveiculos/controller/UsuarioController.java
+++ Locadoraveículos/src/main/java/br/senac/sp/Locadoraveiculos/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,18 @@ public class UsuarioController {
         }
 
         return ResponseEntity.notFound().build();
+
+    }
+
+    //endpoint para deletar um usuario da tabela(endPOints.delete)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirUsuario(@PathVariable("id") Long id){
+
+        repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+
+
 
     }
 
